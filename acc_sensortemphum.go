d47770b04b1d67da4b1083977ee7b2c4ffc13e62
Gabor Gyorvari <scr34m@gmail.com>
2021-02-02 07:23:36 +0100

    set temperature negative end, update at start and after every minutes

--- acc_sensortemphum.go
+++ acc_sensortemphum.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
@@ -19,23 +20,26 @@ func NewAccessorySensorTempHum(conf *ConfigurationDevice) *SensorTempHum {
 	acc.Accessory = accessory.New(accessory.Info{Name: conf.Name, SerialNumber: conf.Serialnumber, Manufacturer: conf.Manufacturer, FirmwareRevision: conf.Version}, accessory.TypeHumidifier)
 
 	acc.TemperatureSensor = service.NewTemperatureSensor()
+	acc.TemperatureSensor.CurrentTemperature.SetMinValue(-100)
 	acc.Accessory.AddService(acc.TemperatureSensor.Service)
 
 	acc.HumiditySensor = service.NewHumiditySensor()
 	acc.Accessory.AddService(acc.HumiditySensor.Service)
 
-	ticker := time.NewTicker(time.Second * 2)
+	ticker := time.NewTicker(time.Second * 60)
 	go func() {
-		for _ = range ticker.C {
+		for ; true; <-ticker.C {
 			var result map[string]interface{}
 			err := getJSON(conf.Url, &result)
 			if err == nil {
 				if val, ok := result["temperature"]; ok {
 					f, _ := strconv.ParseFloat(val.(string), 64)
+					fmt.Printf("%v\n", val)
 					acc.TemperatureSensor.CurrentTemperature.SetValue(f)
 				}
 				if val, ok := result["humidity"]; ok {
 					f, _ := strconv.ParseFloat(val.(string), 64)
+					fmt.Printf("%v\n", val)
 					acc.HumiditySensor.CurrentRelativeHumidity.SetValue(f)
 				}
 			}
