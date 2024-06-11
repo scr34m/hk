1025839d0977e3ded1548977947a852f88b93e02
Gabor Gyorvari <scr34m@gmail.com>
2024-06-11 08:31:45 +0200

    Heating system using mqtt instead of http poll

--- acc_heating_system.go
+++ acc_heating_system.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
-	"time"
 
 	"github.com/brutella/hc/accessory"
 	"github.com/brutella/hc/log"
@@ -21,12 +19,16 @@ type HeatingSystem struct {
 	url          string
 }
 
-func (s *HeatingSystem) pub(name string, payload interface{}) {
-	token := s.mqtt_cli.Publish("hk/"+s.internalname+"/"+name, 0, true, fmt.Sprintf("%v", payload))
-	token.Wait()
-	if token.Error() != nil {
-		log.Info.Println(token.Error())
-	}
+func (s *HeatingSystem) OnForwardSet(client mqtt.Client, msg mqtt.Message) {
+	value, _ := strconv.ParseFloat(string(msg.Payload()), 64)
+	log.Info.Printf("OnForwardSet %v\n", value)
+	s.TemperatureForwardSensor.CurrentTemperature.SetValue(value)
+}
+
+func (s *HeatingSystem) OnBackwardSet(client mqtt.Client, msg mqtt.Message) {
+	value, _ := strconv.ParseFloat(string(msg.Payload()), 64)
+	log.Info.Printf("OnBackwardSet %v\n", value)
+	s.TemperatureBackwardSensor.CurrentTemperature.SetValue(value)
 }
 
 func (s *HeatingSystem) init(internalname string, mqtt_cli mqtt.Client, url string) {
@@ -34,25 +36,17 @@ func (s *HeatingSystem) init(internalname string, mqtt_cli mqtt.Client, url stri
 	s.mqtt_cli = mqtt_cli
 	s.url = url
 
-	ticker := time.NewTicker(time.Second * 60)
-	go func() {
-		for ; true; <-ticker.C {
-			var result map[string]interface{}
-			err := getJSON(s.url, &result)
-			if err == nil {
-				if val, ok := result["forward"]; ok {
-					f, _ := strconv.ParseFloat(val.(string), 64)
-					s.TemperatureForwardSensor.CurrentTemperature.SetValue(f)
-					s.pub("forward", f)
-				}
-				if val, ok := result["backward"]; ok {
-					f, _ := strconv.ParseFloat(val.(string), 64)
-					s.TemperatureBackwardSensor.CurrentTemperature.SetValue(f)
-					s.pub("backward", f)
-				}
-			}
-		}
-	}()
+	topic := "hk/" + s.internalname + "/forward"
+	token := s.mqtt_cli.Subscribe(topic, 1, s.OnForwardSet)
+	token.Wait()
+
+	log.Info.Printf("Subscribed to topic: %s\n", topic)
+
+	topic = "hk/" + s.internalname + "/backward"
+	token = s.mqtt_cli.Subscribe(topic, 1, s.OnBackwardSet)
+	token.Wait()
+
+	log.Info.Printf("Subscribed to topic: %s\n", topic)
 }
 
 func NewAccessoryHeatingSystem(conf *ConfigurationDevice, mqtt_cli mqtt.Client) *HeatingSystem {
